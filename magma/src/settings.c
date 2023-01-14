5a3af373c4cddd6082ebc0007c80daf1489b867e
bramtechs <bramtech@telenet.be>
2023-01-14 13:08:00 +0100

    ini saving

--- magma/src/settings.c
+++ magma/src/settings.c
@@ -3,9 +3,39 @@
 MagmaSettings Settings = { 0 };
 
 void SaveMagmaSettings(){
-    INFO("TODO: save magma settings");
+    ClearConfig(MAGMA_CONF_PATH);
+    PutBool(MAGMA_CONF_PATH, "unlockFrameRate",Settings.unlockFrameRate);
+    PutBool(MAGMA_CONF_PATH, "skipIntro",Settings.skipIntro);
 }
 
 void LoadMagmaSettings(){
-    INFO("TODO: load magma settings");
+}
+
+void ClearConfig(const char* file){
+    if (!SaveFileText(MAGMA_CONF_PATH,"")){
+        ERROR("Failed to reset save config");
+    }
+}
+
+inline void PutBool(const char* file, const char* name, bool on){
+    PutField(file, name,on ? "true":"false");
+}
+
+void PutField(const char* file, const char* name, const char* value){
+    int result = mkdir("../save", 0777);
+
+    char* content = LoadFileText(file);
+    const char* field = TextFormat("%s = %s",name,value);
+    
+    if (content != NULL){
+        const char* newContent = TextFormat("%s\n%s",content,field);
+        if (!SaveFileText(file, (char*) newContent)){
+            ERROR("Failed to put field '%s' with data '%s' in file '%s'",name,value,file);
+        }
+    }else{
+        if (!SaveFileText(file, (char*) field)){
+            ERROR("Failed to put field '%s' with data '%s' in file '%s'",name,value,file);
+        }
+    }
+    UnloadFileText(content);
 }
--- magma/src/settings.h
+++ magma/src/settings.h
@@ -2,6 +2,10 @@
 
 // dump all globals in here lol
 #include "magma.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define MAGMA_CONF_PATH "../save/engine_conf.ini"
 
 typedef struct {
     bool unlockFrameRate;
@@ -12,3 +16,8 @@ extern MagmaSettings Settings;
 
 void SaveMagmaSettings();
 void LoadMagmaSettings();
+
+void ClearConfig(const char* file);
+
+inline void PutBool(const char* file, const char* name, bool on);
+void PutField(const char* file, const char* name, const char* value);
