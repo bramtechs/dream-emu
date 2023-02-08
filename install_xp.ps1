9d5ca1c8d583c81690c04f358709cd772f1dcb09
bramtechs <bramtech@telenet.be>
2023-02-08 13:30:24 +0100

    try to get installer working

--- install_xp.ps1
+++ install_xp.ps1
@@ -1,15 +1,1 @@
-$OriginalLocation = Get-Location
-Set-Location "C:\Program Files (x86)\Microsoft Visual Studio\Installer\"
-$InstallPath = "C:\Program Files\Microsoft Visual Studio\2022\Preview"
-$WorkLoads = '--add Microsoft.VisualStudio.Component.WinXP'
-$Arguments = ('/c', "vs_installer.exe", 'modify', '--installPath', "`"$InstallPath`"", $WorkLoads, '--quiet', '--norestart', '--nocache')
-$process = Start-Process -FilePath cmd.exe -ArgumentList $Arguments -Wait -PassThru -WindowStyle Hidden
-if ($process.ExitCode -eq 0)
-{
-    Write-Host "components have been successfully added"
-}
-else
-{
-    Write-Host "components were not installed"
-}
-Set-Location $OriginalLocation
+& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath 'C:\Program Files\Microsoft Visual Studio\2022\Preview' --add 'Microsoft.VisualStudio.Component.WinXP' --quiet --norestart --nocache --force
