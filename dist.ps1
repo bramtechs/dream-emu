18c2128b8edbed5e33139a706efdf052b7b30933
bramtechs <bramtech@telenet.be>
2023-02-08 13:33:25 +0100

    pleas work

--- dist.ps1
+++ dist.ps1
@@ -1,9 +1,11 @@
 $BUILD_DIR= ".\build\dream\Release"
 $ErrorActionPreference = "Stop"
 
+# install windows xp compilation support
+& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath 'C:\Program Files\Microsoft Visual Studio\2022\Preview' --add 'Microsoft.VisualStudio.Component.WinXP' --quiet --norestart --nocache --force
+
 # download all dependencies
 .\deps.ps1
-.\install_xp.ps1
  
 # build everything in release mode
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 17 2022" -A x64 -T v141_xp
--- install_xp.ps1
+++
@@ -1,1 +0,0 @@
-& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath 'C:\Program Files\Microsoft Visual Studio\2022\Preview' --add 'Microsoft.VisualStudio.Component.WinXP' --quiet --norestart --nocache --force
